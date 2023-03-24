8b7df095229678720623df800d71eeb8ac903292
“chandur-surge” <chandur@surge.global>
2023-03-25 02:28:09 +0530

    Added commit to All Pets

--- app/screens/Pet/AllPets.js
+++ app/screens/Pet/AllPets.js
@@ -1,3 +1,8 @@
+/**
+ * @description This component displays a list of all pets.
+ * @params {object} route - Contains the parameters passed to this component through the navigation object.
+*/
+
 import { useEffect, useState } from 'react';
 import { useIsFocused } from '@react-navigation/native';
 
