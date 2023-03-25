ac697e9ffddc1bdbd22146379ff42b590fae120a
“chandur-surge” <chandur@surge.global>
2023-03-25 08:40:03 +0530

    Modified the home page

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -3,88 +3,88 @@ import { StatusBar } from 'expo-status-bar';
 import { View, Text, StyleSheet, TouchableNativeFeedback, Image } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { Fontisto } from '@expo/vector-icons';
+import { ScrollView } from 'react-native';
 const Home = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Text style={styles.heading}>Friendly Paws</Text>
+        </View>
+        <Text style={styles.heading2}>We provide everything you need to keep your pet happy, healthy and </Text>
         <Image
           resizeMode="contain"
           source={require('../assets/logo.png')}
           style={styles.image}
-        ></Image>
-        <Text style={styles.heading}>Friendly Paws</Text>
-        <Image
-          resizeMode="contain"
-          source={require('../assets/logo.png')}
-          style={styles.image}
-        ></Image>
-      </View>
+        />
 
-      <View style={styles.cardContainer}>
-        <TouchableNativeFeedback onPress={() => navigation.navigate('Channel Doctor')}>
-          <View style={[styles.card, styles.firstCard]}>
-            <Fontisto name="doctor" size={28} color="black" style={styles.cardIcon} />
-            <Text style={styles.cardTitle}>Chanelling</Text>
-          </View>
-        </TouchableNativeFeedback>
-        {/* <TouchableNativeFeedback onPress={() => navigation.navigate('StaffManagement')}>
+        <View style={styles.cardContainer}>
+          <TouchableNativeFeedback onPress={() => navigation.navigate('Channel Doctor')}>
+            <View style={[styles.card, styles.firstCard]}>
+              <Fontisto name="doctor" size={28} color="black" style={styles.cardIcon} />
+              <Text style={styles.cardTitle}>Chanelling</Text>
+            </View>
+          </TouchableNativeFeedback>
+          {/* <TouchableNativeFeedback onPress={() => navigation.navigate('StaffManagement')}>
           <View style={styles.card}>
             <MaterialIcons name="feedback" size={28} color="black" style={styles.cardIcon} />
             <Text style={styles.cardTitle}>User Reviews</Text>
           </View>
         </TouchableNativeFeedback> */}
-        <TouchableNativeFeedback onPress={() => navigation.navigate('StaffManagement')}>
-          <View style={styles.card}>
-            <FontAwesome5 name="hospital-user" size={28} color="black" style={styles.cardIcon} />
-            <Text style={styles.cardTitle}>Manage Staff</Text>
-          </View>
-        </TouchableNativeFeedback>
-        <TouchableNativeFeedback
-          onPress={() => navigation.navigate("AllPets")}
-        >
-          <View style={[styles.card, styles.lastCard]}>
-            <FontAwesome5 name="dog" size={28} color="red" style={styles.cardIcon} />
-            <Text style={styles.cardTitle}>Pet Adopt</Text>
-          </View>
-        </TouchableNativeFeedback>
-        <TouchableNativeFeedback
-          onPress={() => navigation.navigate('View Channelings')}
-        >
-          <View style={[styles.card, styles.lastCard]}>
-            <FontAwesome5 name="dog" size={28} color="red" style={styles.cardIcon} />
-            <Text style={styles.cardTitle}>View Channelings</Text>
-          </View>
-        </TouchableNativeFeedback>
-        <TouchableNativeFeedback onPress={() => navigation.navigate('Bid')}>
-          <View style={styles.card}>
-            <FontAwesome5 name="dog" size={28} color="red" style={styles.cardIcon} />
-            <Text style={styles.cardTitle}>Place Bid</Text>
-          </View>
-        </TouchableNativeFeedback>
-      </View>
+          <TouchableNativeFeedback onPress={() => navigation.navigate('StaffManagement')}>
+            <View style={styles.card}>
+              <FontAwesome5 name="hospital-user" size={28} color="black" style={styles.cardIcon} />
+              <Text style={styles.cardTitle}>Manage Staff</Text>
+            </View>
+          </TouchableNativeFeedback>
+          <TouchableNativeFeedback
+            onPress={() => navigation.navigate("AllPets")}
+          >
+            <View style={[styles.card, styles.lastCard]}>
+              <FontAwesome5 name="dog" size={28} color="red" style={styles.cardIcon} />
+              <Text style={styles.cardTitle}>Pet Adopt</Text>
+            </View>
+          </TouchableNativeFeedback>
+          <TouchableNativeFeedback
+            onPress={() => navigation.navigate('View Channelings')}
+          >
+            <View style={[styles.card, styles.lastCard]}>
+              <FontAwesome5 name="dog" size={28} color="red" style={styles.cardIcon} />
+              <Text style={styles.cardTitle}>View Channelings</Text>
+            </View>
+          </TouchableNativeFeedback>
+          <TouchableNativeFeedback onPress={() => navigation.navigate('Bid')}>
+            <View style={styles.card}>
+              <FontAwesome5 name="dog" size={28} color="red" style={styles.cardIcon} />
+              <Text style={styles.cardTitle}>Place Bid</Text>
+            </View>
+          </TouchableNativeFeedback>
+        </View>
 
-      <StatusBar style="auto" />
-    </View>
+        <StatusBar style="auto" />
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f7ad19',
+    backgroundColor: '#D3D3D3',
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'column',
+    paddingBottom: 40
   },
   heading: {
     fontSize: 40,
     fontWeight: '700',
-    margin: 5,
+    marginTop: 10,
     color: '#053f5c',
   },
   buttonContainer: {
     height: 40,
-    width: '100%',
+    width: '90%',
     borderRadius: 10,
     marginLeft: 50,
     marginRight: 50,
@@ -143,8 +143,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   image: {
-    width: '15%',
-    borderBottomRightRadius: -10,
+    width: '100%',
+  },
+  heading2: {
+    fontSize: 15,
+    color: '#053f5c',
+    paddingBottom: 20,
+    margin: 10,
+    textAlign: 'center'
   },
 });
 
